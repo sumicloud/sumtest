1f9f78138fa5f577e38272bf0e45543300953da9
sumicloud <53965767+sumicloud@users.noreply.github.com>
2019-09-27 16:14:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "aws_instance" "ubuntu" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   availability_zone           = "${var.aws_region}b"
+  count = 2
   associate_public_ip_address = "true"
   tags = {
     Name  = var.name
