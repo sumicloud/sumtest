cd54f13b16c09ffdde5a97e9366a31572897f174
sumicloud <53965767+sumicloud@users.noreply.github.com>
2019-09-27 16:39:53 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "name" {
   description = "name to pass to Name tag"
-  default     = "SE Hangout Demo2"
+  default     = "SE Hangout Demo3"
 }
 
 variable "instance_count" {
